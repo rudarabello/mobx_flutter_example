bf2e542804c5039e13cc9de35d05875a9876e553
rudarabello <ruda.rabello.silva@gmail.com>
2022-12-11 15:41:31 -0300

    feat: add states

---
+++ lib/home_states.dart
@@ -0,0 +1,9 @@
+abstract class HomeState {}
+
+class HomeInitialState implements HomeState {}
+
+class HomeLoadingState implements HomeState {}
+
+class HomeSuccessState implements HomeState {}
+
+class HomeErrorState implements HomeState {}
